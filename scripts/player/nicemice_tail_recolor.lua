9f61dc17cf4d7f133014af72ea9b81b5dc5ff8ab
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2025-05-26 16:29:50 -0500

    correct mouth position for breathing checks

--- scripts/player/nicemice_tail_recolor.lua
+++ scripts/player/nicemice_tail_recolor.lua
@@ -5,6 +5,16 @@ local _uninit = uninit
 function init()
 	_init()
 	
+	-- +0.5 is where drowning begins
+	-- -2.5 is where drowning begins
+	-- we can infer from these numbers that -1.25 is our center
+	-- the boundingbox encompasses 22 pixels vertically coming upward from our feet
+	-- mouth position is +16 pixels from our feet
+	-- 16 / 22 = 0.727272 R
+	-- 72% / 2 = 36
+	-- 36% of 3.0 = 1.08
+	-- -1.25 + 1.08 = -0.17
+	status.setStatusProperty("mouthPosition",{0,-0.17})
 end
 
 function update(dt)

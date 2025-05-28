15cc43f0448237a3c864e981cc2423f156fe8b4e
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2025-05-28 00:40:21 -0500

    Nicemice crew members avoid Captain's Chair

--- npcs/crew/nicemice_scriptedCrewMemberBehavior.lua
+++ npcs/crew/nicemice_scriptedCrewMemberBehavior.lua
@@ -9,4 +9,59 @@ local _update = update
 function update(dt)
 	_update(dt)
 	
+	avoidCaptainsChair(dt)
+end
+
+local timeSinceLastCaptainChairScan = 999
+function avoidCaptainsChair(dt)
+--  scan every 5 sec
+	timeSinceLastCaptainChairScan = timeSinceLastCaptainChairScan + dt
+	if timeSinceLastCaptainChairScan > 3 then
+	
+		--  reset timer
+		timeSinceLastCaptainChairScan = 0
+		
+		--  query nearby objects
+		local objects = world.objectQuery(mcontroller.position(),10)
+		if objects then
+		
+			--  iterate results
+			for _, objectId in pairs(objects) do
+			
+				--  check object tags
+				local tags = world.getObjectParameter(objectId,"itemTags")
+				if tags then
+				
+					-- has tags, iterate
+					for _, tag in ipairs(tags) do
+					
+						--  if it's the captain's chair
+						if tag == "captainschair" then
+							
+							--  get away from it
+							local chairPos  = world.entityPosition(objectId)
+							if chairPos ~= nil then
+							
+								local curPos = mcontroller.position()
+								if curPos ~= nil then
+									
+									--  if we're to the right of the chair, run right
+									if chairPos[1] < curPos[1] then
+										nicemice_setNPCBehavior("nicemice_crew_avoidCaptainsChair_npcRunRight")
+										
+									--  otherwise run left
+									else
+										nicemice_setNPCBehavior("nicemice_crew_avoidCaptainsChair_npcRunLeft")
+									end
+									
+									--  we're done with this loop now
+									break
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
 end
--- npcs/nicemice_npcHooks.lua
+++ npcs/nicemice_npcHooks.lua
@@ -52,3 +52,96 @@ function nicemice_initHooks(args, board)
 
 	return true
 end
+
+function nicemice_npc_move(args, board, node)
+	local bounds = mcontroller.boundBox()
+
+	startTime = os.clock()
+	while true do
+
+		--  exit movement if we hit our timeout
+		if args.timeout ~= nil then
+			if (os.clock() - startTime) > args.timeout then
+				return true
+			end
+		end
+
+		--  not sure if we really need the 'force walking backwards' flag for the applications this is used for. TODO test
+		local direction = util.toDirection(args.direction)
+		local run = args.run
+		if config.getParameter("pathing.forceWalkingBackwards", false) then
+			if run == true then 
+				run = mcontroller.movingDirection() == mcontroller.facingDirection() 
+			end
+		end
+
+		--  this probably softlocks the behavior, but it shouldn't happen. TODO test
+		if args.direction == nil then 
+			return false 
+		end
+		
+		local position = mcontroller.position()
+		--  align bottom of the bound box with the ground
+		position = {position[1], math.ceil(position[2]) - (bounds[2] % 1)} 
+
+		local move = false
+		--  Check for walls
+		for _,yDir in pairs({0, -1, 1}) do
+		--  util.debugRect(rect.translate(bounds, vec2.add(position, {direction * 0.2, yDir})), "yellow")
+			if not world.rectTileCollision(rect.translate(bounds, vec2.add(position, {direction * 0.2, yDir}))) then
+				move = true
+				break
+			end
+		end
+
+		-- Also specifically check for a dumb collision geometry edge case where the ground goes like:
+		--
+		--        #
+		-- ###### ######
+		-- #############
+		local boundsEnd = direction > 0 and bounds[3] or bounds[1]
+		local wallPoint = {position[1] + boundsEnd + direction * 0.5, position[2] + bounds[2] + 0.5}
+		local groundPoint = {position[1] + boundsEnd - direction * 0.5, position[2] + bounds[2] - 0.5}
+		if world.pointTileCollision(wallPoint) and not world.pointTileCollision(groundPoint) then
+			move = false
+		end
+
+		if args.respectLedges then
+			-- Check for ground for the entire length of the bound box
+			-- Makes it so the entity can stop before a ledge
+			if move then
+				local boundWidth = bounds[3] - bounds[1]
+				local groundRect = rect.translate({bounds[1], bounds[2] - 1.0, bounds[3], bounds[2]}, position)
+				local y = 0
+				for x = boundWidth % 1, math.ceil(boundWidth) do
+					move = false
+					for _,yDir in pairs({0, -1, 1}) do
+						--util.debugRect(rect.translate(groundRect, {direction * x, y + yDir}), "blue")
+						if world.rectTileCollision(rect.translate(groundRect, {direction * x, y + yDir}), {"Null", "Block", "Dynamic", "Platform"}) then
+							move = true
+							y = y + yDir
+							break
+						end
+					end
+					if move == false then 
+						break end
+					end
+				end
+			end
+
+			if move then
+			moved = true
+			mcontroller.controlMove(direction, run)
+			if not self.setFacingDirection then 
+				controlFace(direction) 
+			end
+		else
+			if moved then
+				mcontroller.setXVelocity(0)
+				mcontroller.clearControls()
+			end
+			return true
+		end
+		coroutine.yield()
+	end
+end
